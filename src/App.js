8f7305bce66db8adf45c4635ce63b52ef35133c2
Dohyeong Kim <oskakim@aiming-inc.com>
2021-05-10 09:04:00 +0900

    add about

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route, Router } from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 
 function App() {
   return (
     <HashRouter>
-      <Route path ="/" component={Home} />
-      <Route path ="/about" component={About} />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
     </HashRouter>
   );
 }
